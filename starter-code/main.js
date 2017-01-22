cf168bcaec2f121f130a2fe8a08e6dc768b9de76
Hannah Wynn Jones <hannahwynnjones@Hannahs-MacBook-Air.local>
2017-01-22 14:21:01 +0100

    added my changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,26 +4,36 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert("Sorry, try again.");
-}
+//if (cardOne === cardTwo) {
+//	alert("Sorry, try again.");
+//}
 
-else if (cardOne === cardFour) {
-	alert("Sorry, try again.");
-}
+//else if (cardOne === cardFour) {
+//	alert("Sorry, try again.");
+//}
 
-else if (cardOne === cardThree) {
-	alert("You have found a match!");
-}
+//else if (cardOne === cardThree) {
+//	alert("You have found a match!");
+//}
 
-else if (cardTwo === cardThree) {
-	alert("Sorry, try again.");
-}
+//else if (cardTwo === cardThree) {
+//	alert("Sorry, try again.");
+//}
 
-else if (cardTwo === cardFour) {
-	alert("You have found a match!");
-}
+//else if (cardTwo === cardFour) {
+//	alert("You have found a match!");
+//}
 
-else if (cardThree === cardFour) {
-	alert("Sorry, try again.");
-}
+//else if (cardThree === cardFour) {
+//	alert("Sorry, try again.");
+//}
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		
+var cardElement = document.createElement('div');
+
+    cardElement.className = 'card';}
+
